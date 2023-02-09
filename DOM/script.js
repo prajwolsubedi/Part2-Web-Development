fd16080df6211f9acb18e9bbcacd3ad28d143f5e
prajwol <prajwolsubedizzz@gmail.com>
2023-02-09 11:09:37 +0545

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+// alert('Hello');
+let ul = document.querySelector("ul");
+ul.children[2].innerHTML = "prajwol";
+
+// document.querySelector("#title").style.color = "purple";
+document.querySelector(".list a").style.color = "green";
+
+document.querySelector(".btn").style.backgroundColor = "yellow";
