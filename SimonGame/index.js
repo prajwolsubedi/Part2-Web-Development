5b5910d1a20a8c46e4a801c9e25bab93ade232a6
github-personal <prajwolsubedizzz@gmail.com>
2023-02-17 08:55:04 +0545

    simon game

--- SimonGame/index.js
+++ SimonGame/index.js
@@ -1,91 +1,88 @@
-let buttonColors = ["red","blue","green","yellow"];
+let colors = ["blue", "red", "yellow", "green"];
 let gamePattern = [];
-let userClickedPattern = [];
-let randomColor;
-let started = false;
+let userPattern = [];
 let level = 0;
 let heading = $("#level-title");
+let body = $("body");
+let allButton = $(".btn");
+let start = 0
 
-//Reset the values of level, gamePattern and started variables.
 function restart(){
     gamePattern = [];
-    started = false;
+    userPattern = [];
     level = 0;
+    start = 0;
+}
+
+//When pressed any button start the game but do it only once
+if(!start){
+    body.keydown(function(){
+        heading.text("Level " + level);
+        start = 1;
+        nextSequence();
+    });
 }
 
 
-    //function to produce sound
-    function playSound(name) {
-
-        // Take the code we used to play sound in the nextSequence() function and add it to playSound().
-        let audio = new Audio("sounds/" + name + ".mp3");
-        audio.play();
-      }
-
-
-$(document).keydown(function(){
-    if(!started){
-        nextSequence();
-        started = true;
-    }
+allButton.click(function(e){
+    let userChosenColour = $(this).attr("id");
+    userPattern.push(userChosenColour);
+    playAnimation(userChosenColour);
+    playSound(userChosenColour);
+    checkAnswer(userPattern.length - 1);
 })
 
-function checkAnswer(currLevel){
-    if(userClickedPattern[currLevel] === gamePattern[currLevel]){
-    console.log("success");
 
-    //If the sequence if finished produce next sequence
-    if(userClickedPattern.length === gamePattern.length){
-        setTimeout(function(){
-            nextSequence();
-        },1000)
+function checkAnswer(currLevel){
+
+    if(userPattern[currLevel] === gamePattern[currLevel]){
+        console.log("success");
+        //Check if all userPattern is already scanned
+        if(userPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000)
+        }
     }
-}
-    else {
-        $("body").addClass("game-over");
+    else{
+        heading.text("Game Over Press any Key to continue again.");
+        body.addClass("game-over");
         playSound("wrong");
-        $(heading).text("Game Over Press Any Key To Restart.")
         setTimeout(function(){
-        $("body").removeClass("game-over");
+            body.removeClass("game-over");
         },300)
         restart();
     }
-}
-
-
-let allBtn = $(".btn");
-allBtn.click(function(){
-    let userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-    checkAnswer(userClickedPattern.length-1);
-});
-
-
-function animatePress(currentColor){
-    let active = $("#" + currentColor);
-    active.addClass("pressed");
-    setTimeout(function(){
-        active.removeClass("pressed");
-    },100)
+    
 }
 
 function nextSequence(){
-    // Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
-    userClickedPattern = [];
+    userPattern = [];
     level++;
-    $(heading).text("Level " + level);
-    let n = Math.random();
-    let randomNumber = Math.floor(n*4);
-    let randomColor = buttonColors[randomNumber];
+    heading.text("Level " + level);
+    let x = Math.random();
+    let random = Math.floor(x*4);
+    let randomColor = colors[random];
     gamePattern.push(randomColor);
-    let active = $("#"+randomColor);
-    $(active).fadeOut(200).fadeIn(200);
-    // produceSound(randomColor)
     playSound(randomColor);
-}    
+    playAnimation(randomColor);
+    let active = $("#"+randomColor);
+    active.fadeOut(100).fadeIn(100);
+}
+
+//Play Animation
+function playAnimation(active){
+    let activeClass = $("." + active);
+    activeClass.addClass("pressed");
+    setTimeout(function(){
+        activeClass.removeClass("pressed");
+    },100)
+}
 
 
-
-
+//Play Sound
+function playSound(name){
+    let audio = new Audio;
+    audio.src = "sounds/" + name + ".mp3";
+    audio.play();
+}
--- SimonGame/tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-                generateColor(),7000
